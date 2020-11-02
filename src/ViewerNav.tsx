6df270ffe32cc0d86f67d920f54dfc4f616239fa
曾艳 <zengy@zjcds.com>
2020-11-02 09:43:42 +0800

    zy

--- src/ViewerNav.tsx
+++ src/ViewerNav.tsx
@@ -11,20 +11,20 @@ export interface ViewerNavProps {
 
 export default function ViewerNav(props: ViewerNavProps) {
   const { activeIndex = 0, navImgWidth = 100 } = props;
-  const [marginValue, setMarginValue] = React.useState(0)
-  const ulRef = React.useRef()
-  const [showNext, setShowNext] = React.useState(false)
+  const [marginValue, setMarginValue] = React.useState(0);
+  const ulRef = React.useRef();
+  const [showNext, setShowNext] = React.useState(false);
 
-  React.useEffect(()=>{
-    let ulContainer = ulRef.current || undefined
-    let ulWidth = ulContainer.clientWidth
-    const showNextButton = (navImgWidth+10)*props.images.length+marginValue>ulWidth
-    if(showNextButton) {
-      setShowNext(true)
+  React.useEffect(() => {
+    let ulContainer = ulRef.current || undefined;
+    let ulWidth = ulContainer.clientWidth;
+    const showNextButton = (navImgWidth + 10) * props.images.length + marginValue > ulWidth;
+    if (showNextButton) {
+      setShowNext(true);
     } else {
-      setShowNext(false)
+      setShowNext(false);
     }
-  })
+  });
 
   function handleChangeImg(newIndex) {
     if (activeIndex === newIndex) {
@@ -34,41 +34,41 @@ export default function ViewerNav(props: ViewerNavProps) {
   }
 
   function goNext() {
-    let currentValue = marginValue-navImgWidth-10
-    setMarginValue(currentValue)
+    let currentValue = marginValue - navImgWidth - 10;
+    setMarginValue(currentValue);
   }
 
   function goPre() {
-    let currentValue = marginValue+navImgWidth+10
-    setMarginValue(currentValue)
+    let currentValue = marginValue + navImgWidth + 10;
+    setMarginValue(currentValue);
   }
 
   let listStyle = {
-    left: marginValue
+    left: marginValue,
   };
 
   let liStyle = {
     width: navImgWidth,
-  }
+  };
 
   return (
     <div className={`${props.prefixCls}-navbar divContainer`}>
       {
         marginValue ? <a className="preButton" onClick={() => goPre()}>上一张</a> : <span className="preButton"></span>
       }
-      <div className="ulContainer" ref={ulRef} style={{margin: 'auto', width: (navImgWidth+10)*props.images.length}}>
+      <div className="ulContainer" ref={ulRef} style={{margin: 'auto', width: (navImgWidth + 10) * props.images.length}}>
       <ul className={`${props.prefixCls}-list ${props.prefixCls}-list-transition`} style={listStyle}>
-          {props.images.map((item, index) =>
-            <li
-            key={index}
-            className={index === activeIndex ? 'active' : ''}
-            style={liStyle}
-            onClick={() => { handleChangeImg(index); }}
-            >
-              <img src={item.src} alt={item.alt} style={{width: navImgWidth}}/>
-            </li>,
-            )
-          }
+        {props.images.map((item, index) =>
+          <li
+          key={index}
+          className={index === activeIndex ? 'active' : ''}
+          style={liStyle}
+          onClick={() => { handleChangeImg(index); }}
+          >
+            <img src={item.src} alt={item.alt} style={{width: navImgWidth}}/>
+          </li>,
+          )
+        }
         </ul>
       </div>
       {
